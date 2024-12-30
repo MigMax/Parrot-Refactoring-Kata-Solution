5d07984efc83604b56ebb9a36251fd063e5de09c
MigMax <maxence.martin.info@gmail.com>
2024-12-30 17:12:55 +0100

    Extract classe NorwegianBlueParrot

---
+++ CSharp/Parrot/NorwegianBlueParrot.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Parrot;
+
+public class NorwegianBlueParrot(double voltage, bool isNailed) : Parrot(ParrotTypeEnum.NORWEGIAN_BLUE, voltage, isNailed)
+{
+    public override double GetSpeed()
+    {
+        return isNailed ? 0 : GetBaseSpeed();
+    }
+
+    private double GetBaseSpeed()
+    {
+        return Math.Min(24.0, voltage * BaseSpeed);
+    }
+    
+    public override string GetCry()
+    {
+        return voltage > 0 ? "Bzzzzzz" : "...";
+    }
+}
--- CSharp/Parrot/Parrot.cs
+++ CSharp/Parrot/Parrot.cs
@@ -4,27 +4,20 @@ namespace Parrot;
 
 public class Parrot(ParrotTypeEnum type, double voltage, bool isNailed)
 {
+    protected const double BaseSpeed = 12.0;
+    
     public virtual double GetSpeed()
     {
         return type switch
         {
-            ParrotTypeEnum.NORWEGIAN_BLUE => isNailed ? 0 : GetBaseSpeed(voltage),
             _ => throw new ArgumentOutOfRangeException()
         };
     }
-
-    private static double GetBaseSpeed(double voltage)
-    {
-        return Math.Min(24.0, voltage * BaseSpeed);
-    }
-    
-    protected const double BaseSpeed = 12.0;
     
     public virtual string GetCry()
     {
         return type switch
         {
-            ParrotTypeEnum.NORWEGIAN_BLUE => voltage > 0 ? "Bzzzzzz" : "...",
             _ => throw new ArgumentOutOfRangeException()
         };
     }
